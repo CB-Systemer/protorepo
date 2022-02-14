03a30eab9a54360ce7de864abf6259dd999eb874
Nicolaj Iversen <nicolaj.iversen@gmail.com>
2022-02-14 12:30:42 +0100

    moves setprotopath

--- scripts/setProtoPath.ps1
+++ scripts/setProtoPath.ps1
@@ -1,11 +1,12 @@
 if($IsLinux){
-    $protoRepoPath = $(Get-Location).Path;
+    $protoRepoPath = Split-Path -Path $(Get-Location).Path -Parent;
     sh & "export PROTOREPO='$protoRepoPath'" >> ~/.profile
     Write-Host "export PROTOREPO='$protoRepoPath' added to your .profile" -ForegroundColor Green
 }
 
 if($IsWindows){
-    $protoRepoPath = Get-Location;
+    $dir = Get-Location;
+    $protoRepoPath = Split-Path -Path $dir.Path -Parent;
     [Environment]::SetEnvironmentVariable("PROTOREPO", $protoRepoPath.Path, "Machine")
     
     $path = $protoRepoPath.Path;
